ff2dc78451c2d3d5515d5a8b58e56d38cd751ce4
Your Name <your-email@example.comnpm>
2025-11-18 01:36:52 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes, Route, useLocation,useNavigate } from 'react-router-dom';
+import SignUp from './component/Authentication/SignUp';
 
-function App() {
+
+function AppContent() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* {loading ? (
+        <LoadingSpinner /> // Show only the spinner until loading is complete
+      ) : ( */}
+      <Routes>
+        <Route path='/' element={<SignUp />} />
+      </Routes>
+      {/* )} */}
+    </>
+      
   );
 }
 
+function App() {
+
+  return (
+    <div className="App">
+       <HashRouter>
+        <AppContent />
+      </HashRouter>
+    </div>
+  );
+}
 export default App;
+// git add .
+// git commit -m "Your commit message"
+// git push
