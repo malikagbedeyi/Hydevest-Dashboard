184b526cdd09afc763973d1790f4c64460b9b68e
Ancestor <malik@Agbedeyis-MacBook-Pro.local>
2026-03-18 23:15:03 +0100

    repush discount update

--- src/component/Dashboard/SaleComponent/Sale/SaleTable.jsx
+++ src/component/Dashboard/SaleComponent/Sale/SaleTable.jsx
@@ -70,7 +70,7 @@ const confirmDelete = () => {
                 <th>Customer Name</th>
                 <th>Customer Phone</th> 
                 <th>Total Sale Amount</th>
-                <th>Tota Discount</th>
+                <th>Total Discount</th>
                 <th>Total Amount Paid</th>
                  <th>Outstanding Balance</th>
                  <th>Created By</th>
